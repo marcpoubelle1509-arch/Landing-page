aa102478e7f17a4d3314f40a88741d07ef25190e
marcpoubelle1509-arch <marcpoubelle1509@gmail.com>
2025-08-20 21:09:19 +0000

    ff# Please enter the commit message for your changes. Lines starting

--- vite.config.js
+++
@@ -1,8 +0,0 @@
-import { defineConfig } from 'vite';
-
-export default defineConfig({
-  root: '.', // Définit la racine du projet
-  build: {
-    outDir: 'dist', // Dossier de sortie du build
-  },
-});
